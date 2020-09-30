dc7fa3d199feffdfd37f473ce4f9b000ba7d480b
Ashley Craig <ashleycraig19872@hotmail.co.uk>
2020-09-30 22:24:09 +0100

    In main Go file add check for arguments and get environment from them

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func checkArguments() {
+	if len(os.Args) < 2 {
+		fmt.Println("Argument not provided, exiting...")
+		os.Exit(1)
+	}
+}
 
 func main() {
+	checkArguments();
+
+	arguments := os.Args[1:];
+
+	environment := arguments[0];
+
 	fmt.Println("Welcome to manifest!");
+	fmt.Println("Selected environment is: " + environment);
 }
